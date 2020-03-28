930436e3e61e3fa6b14d6ca5f8967eba604510df
Edo Novanto <novantoedo@gmail.com>
2020-03-28 16:57:32 +0700

    construct method

---
+++ app/Http/Controllers/Webhook.php
@@ -0,0 +1,77 @@
+<?php
+ 
+namespace App\Http\Controllers;
+ 
+use App\Gateway\EventLogGateway;
+use App\Gateway\QuestionGateway;
+use App\Gateway\UserGateway;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Log\Logger;
+use LINE\LINEBot;
+use LINE\LINEBot\HTTPClient\CurlHTTPClient;
+use LINE\LINEBot\MessageBuilder\MultiMessageBuilder;
+use LINE\LINEBot\MessageBuilder\StickerMessageBuilder;
+use LINE\LINEBot\MessageBuilder\TemplateBuilder\ButtonTemplateBuilder;
+use LINE\LINEBot\MessageBuilder\TemplateMessageBuilder;
+use LINE\LINEBot\MessageBuilder\TextMessageBuilder;
+use LINE\LINEBot\TemplateActionBuilder\MessageTemplateActionBuilder;
+ 
+class Webhook extends Controller
+{
+    /**
+     * @var LINEBot
+     */
+    private $bot;
+    /**
+     * @var Request
+     */
+    private $request;
+    /**
+     * @var Response
+     */
+    private $response;
+    /**
+     * @var Logger
+     */
+    private $logger;
+    /**
+     * @var EventLogGateway
+     */
+    private $logGateway;
+    /**
+     * @var UserGateway
+     */
+    private $userGateway;
+    /**
+     * @var QuestionGateway
+     */
+    private $questionGateway;
+    /**
+     * @var array
+     */
+    private $user;
+ 
+ 
+    public function __construct(
+        Request $request,
+        Response $response,
+        Logger $logger,
+        EventLogGateway $logGateway,
+        UserGateway $userGateway,
+        QuestionGateway $questionGateway
+    ) {
+        $this->request = $request;
+        $this->response = $response;
+        $this->logger = $logger;
+        $this->logGateway = $logGateway;
+        $this->userGateway = $userGateway;
+        $this->questionGateway = $questionGateway;
+ 
+        // create bot object
+        $httpClient = new CurlHTTPClient(getenv('CHANNEL_ACCESS_TOKEN'));
+        $this->bot  = new LINEBot($httpClient, ['channelSecret' => getenv('CHANNEL_SECRET')]);
+    }
+
+    
+}
