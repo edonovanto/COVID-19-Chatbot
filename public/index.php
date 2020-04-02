d94b3cb1b7c5943c463e1cb4759cc9f0129b15eb
Edo Novanto <novantoedo@gmail.com>
2020-04-02 08:06:45 +0700

    multi message

--- public/index.php
+++ public/index.php
@@ -28,27 +28,30 @@ $app = require __DIR__.'/../bootstrap/app.php';
 $country = "indonesia";
 $flop = "aslkaskdoks";
 
+
+if('https://corona.lmao.ninja/countries/'.$country){
+
     $url = 'https://corona.lmao.ninja/countries/'. $country;
+    $json = file_get_contents($url);
+    echo $json;
 
-    $result = "";
+    // $json = json_decode($json);
 
+    // if ($json->message == "Country not found or doesn't have any cases"){
+    //     echo 'Mohon maaf kami tidak mengerti pesan anda. Silakan kirim pesan "MULAI" untuk memulai kuis atau masukkan nama negara yang sesuai.';
+    // }
+    // else {
+        // $result1 = $json->cases;
+        // $result2 = $json->todayCases;
+        // $result3 = $json->deaths;
+        // $result4 = $json->recovered;
+        // $result5 = $json->casesPerOneMillion;
     
-    if ($url){
-        $json = file_get_contents($url);
-
-        $json = json_decode($json);
-
-        $result1 = $json->cases;
-            $result2 = $json->todayCases;
-            $result3 = $json->deaths;
-            $result4 = $json->recovered;
-            $result5 = $json->casesPerOneMillion;
-            $pic = $json->countryInfo->flag;
-        
-            $result .= $result1 ."\n". $result2 . "\n" . $result3 . "\n" . $result4 . "\n" . $result5 . "\n" . $pic;
-    }
-
-
-            echo $result;
+        // echo $result1 ."\n". $result2 . "\n" . $result3 . "\n" . $result4 . "\n" . $result5;
+    // }
+}
+else{
+    echo 'Mohon maaf kami tidak mengerti pesan anda. Silakan kirim pesan "MULAI" untuk memulai kuis atau masukkan nama negara yang sesuai.';
+}
 
 // $app->run();
