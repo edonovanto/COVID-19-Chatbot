06f1f095ca7e366af1d310cefc5632a718a6766e
Edo Novanto <novantoedo@gmail.com>
2020-04-02 12:49:13 +0700

    news update

--- app/Http/Controllers/Webhook.php
+++ app/Http/Controllers/Webhook.php
@@ -209,43 +209,43 @@ class Webhook extends Controller
                 $this->bot->replyMessage($event['replyToken'], $textMessageBuilder);
                 
             }
-            if (strtolower($userMessage) == 'news'){
-                $curl = curl_init();
+            // if (strtolower($userMessage) == 'news'){
+            //     $curl = curl_init();
 
-                curl_setopt_array($curl, array(
-                    CURLOPT_URL => "https://api.smartable.ai/coronavirus/news/US/",
-                    CURLOPT_RETURNTRANSFER => true,
-                    CURLOPT_FOLLOWLOCATION => true,
-                    CURLOPT_ENCODING => "",
-                    CURLOPT_MAXREDIRS => 10,
-                    CURLOPT_TIMEOUT => 30,
-                    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-                    CURLOPT_CUSTOMREQUEST => "GET",
-                    CURLOPT_HTTPHEADER => array(
-                        "Subscription-Key: 3009d4ccc29e4808af1ccc25c69b4d5d"
-                    ),
-                ));
+            //     curl_setopt_array($curl, array(
+            //         CURLOPT_URL => "https://api.smartable.ai/coronavirus/news/US/",
+            //         CURLOPT_RETURNTRANSFER => true,
+            //         CURLOPT_FOLLOWLOCATION => true,
+            //         CURLOPT_ENCODING => "",
+            //         CURLOPT_MAXREDIRS => 10,
+            //         CURLOPT_TIMEOUT => 30,
+            //         CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            //         CURLOPT_CUSTOMREQUEST => "GET",
+            //         CURLOPT_HTTPHEADER => array(
+            //             "Subscription-Key: 3009d4ccc29e4808af1ccc25c69b4d5d"
+            //         ),
+            //     ));
 
-                $response = curl_exec($curl);
+            //     $response = curl_exec($curl);
 
-                curl_close($curl);
+            //     curl_close($curl);
 
-                $result = json_decode($response, true);
+            //     $result = json_decode($response, true);
 
-                $number = mt_rand(1,29);
+            //     $number = mt_rand(1,29);
 
-                $image = $result['news'][$number]["images"][$number]['url'];
-                $imageMessageBuilder = new ImageMessageBuilder($image, $image);
-                $title = $result['news'][$number]['title'] . "\n";
-                $title .= $result['news'][$number]["webUrl"];
-                $textMessageBuilder = new TextMessageBuilder($title);
+            //     $image = $result['news'][$number]["images"][$number]['url'];
+            //     $imageMessageBuilder = new ImageMessageBuilder($image, $image);
+            //     $title = $result['news'][$number]['title'] . "\n";
+            //     $title .= $result['news'][$number]["webUrl"];
+            //     $textMessageBuilder = new TextMessageBuilder($title);
 
-                $multiMessageBuilder = new MultiMessageBuilder();
-                $multiMessageBuilder->add($imageMessageBuilder);
-                $multiMessageBuilder->add($textMessageBuilder);
+            //     $multiMessageBuilder = new MultiMessageBuilder();
+            //     $multiMessageBuilder->add($imageMessageBuilder);
+            //     $multiMessageBuilder->add($textMessageBuilder);
 
-                $this->bot->replyMessage($event['replyToken'], $multiMessageBuilder);
-            }
+            //     $this->bot->replyMessage($event['replyToken'], $multiMessageBuilder);
+            // }
             if('https://corona.lmao.ninja/countries/'.strtolower($userMessage)){
                 $url = "https://corona.lmao.ninja/countries/".strtolower($userMessage);
                 $json = file_get_contents($url);
